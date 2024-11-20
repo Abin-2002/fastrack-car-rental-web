f6fd273280afb1a1bd6d337db5c5373b199e2c4c
Abin-2002 <abinrnair10@gmail.com>
2024-11-20 15:50:35 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 8000
+const cors = require('cors')
+const connectDB = require('./config/db')
+const colors = require('colors')
+const bodyParser = require("body-parser");
+
+
+const userRoutes = require('./routes/userRouter')
+const adminRoutes = require('./routes/adminRouter')
+const driverRoutes = require('./routes/driverRouter')
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+// Database Connection
+connectDB(()=>{
+    try{
+        console.log("Database Successfully Connected");
+    } catch (error) {
+        console.log("Database Not Connected: ", error);
+    }
+})
+
+// Middlewares
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Routes
+app.use('/api/users',userRoutes)
+app.use('/api/admin',adminRoutes)
+app.use('/api/driver',driverRoutes)
+
+app.use(errorHandler)
+
+
+
+app.listen(port, () =>
+console.log(`Server Started on port ${port}`.yellow.bold));
